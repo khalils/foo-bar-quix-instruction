9aaa43b4b7ebcc8cfb94fa3071afe76be35cd4ef
khalils1 <khalil.sahraoui@gmail.com>
2021-12-01 21:50:15 +0100

    Test with the number 1 should Return 1 (succeeded)

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        return inputNumber.toString()
     }
 
 }
