b2a2cb763463ce5937d70b1e0c36615c28324ae5
khalils1 <khalil.sahraoui@gmail.com>
2021-12-01 21:48:48 +0100

    Test with the number 1 should Return 1 (Failed)

--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
@@ -1,13 +1,22 @@
 package com.kata.foobarquix
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.springframework.boot.test.context.SpringBootTest
+import com.kata.foobarquix.services.FooBarQuixService
 
 @SpringBootTest
 class FooBarQuixApplicationTests {
 
+	val fooBarQuixService = FooBarQuixService();
+	
 	@Test
 	fun contextLoads() {
+	}
+	
+	@Test
+	fun testFooBarQuixShouldReturn1() {
+		assertEquals("1",fooBarQuixService.convertNumber(1))
 	}
 
 }
