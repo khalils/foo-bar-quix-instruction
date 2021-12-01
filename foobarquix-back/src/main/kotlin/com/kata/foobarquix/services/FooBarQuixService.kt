accedc16884225e68c555dad248fefc9bb4a1ea6
khalils1 <khalil.sahraoui@gmail.com>
2021-12-01 21:59:24 +0100

    Test with the number 5 should Return BarBar (succeeded)

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -10,10 +10,14 @@ class FooBarQuixService {
     	val result = StringBuilder()
     	if(inputNumber % 3 == 0){
     		result.append("Foo")
+    	}else if(inputNumber % 5 == 0){
+    		result.append("Bar")
     	}
     	
     	if(inputNumber.toString().contains("3")){
     		result.append("Foo")
+    	}else if(inputNumber.toString().contains("5")){
+    		result.append("Bar")
     	}
     	
         return result.toString().ifEmpty{
