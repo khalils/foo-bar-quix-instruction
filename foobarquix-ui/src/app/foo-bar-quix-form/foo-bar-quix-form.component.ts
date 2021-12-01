67426d15aa9c0f2e699490cfa92280448ce8ff77
khalils1 <khalil.sahraoui@gmail.com>
2021-12-01 22:37:34 +0100

    add Foo Bar Quix Navigation Bar + input & convertion button

--- foobarquix-ui/src/app/foo-bar-quix-form/foo-bar-quix-form.component.ts
+++ foobarquix-ui/src/app/foo-bar-quix-form/foo-bar-quix-form.component.ts
@@ -1,19 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-foo-bar-quix-form',
   templateUrl: './foo-bar-quix-form.component.html'
 })
-export class FooBarQuixFormComponent implements OnInit {
+export class FooBarQuixFormComponent {
 
 
-  constructor() {
+  @Output()
+  submitNumberOutput = new EventEmitter<number>();
+  convertForm = new FormGroup({
+    inputNumber: new FormControl('', { validators: [
+      Validators.required,
+      Validators.pattern(/^[0-9]*$/)
+      ]
+    })
+  });
 
-  }
-
-  ngOnInit(): void {
-  }
 
   submitNumber(): void {
   }
